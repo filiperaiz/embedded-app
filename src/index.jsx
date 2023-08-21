c292836ede654c1c1a5b9b004d8dd31dbfa87786
Filipe Raiz <filipe.raiz@unico.io>
2023-08-21 18:31:30 -0300

    window[RUNNER_KEY]

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
@@ -14,36 +13,27 @@ function App() {
       </header>
       <main>
         <h2>Main App</h2>
-       
       </main>
     </div>
   );
 }
 
-
 const renderApp = () => {
-  
-  
-  const APP_ID = 'app_root'
-  const RUNNER_KEY = `${APP_ID}_runner`
-  
-  
-  
+  const APP_ID = "app_root";
+  const RUNNER_KEY = `${APP_ID}_runner`;
+
   global[RUNNER_KEY] = (targetElementId) => {
-    const root = ReactDOM.createRoot(document.getElementById(targetElementId))
+    const root = ReactDOM.createRoot(document.getElementById(targetElementId));
     root.render(
       <React.StrictMode>
         <App />
       </React.StrictMode>
     );
-  }
+  };
 
   if (!global.embedded) {
-    window[RUNNER_KEY]('root')
-  } 
+    window[RUNNER_KEY]("root");
+  }
+};
 
-}
-
-renderApp()
-
-
+renderApp();
