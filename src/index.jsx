0d4efe2fb704885671ac101796c45ada16fdeb37
Filipe Raiz <filipe.raiz@unico.io>
2023-08-21 22:12:13 -0300

    set config

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import logo from "./logo.svg";
 
-function App() {
+const getAuth = async (credentials) => {
+  const safetyMargin = 5 * 60 * 1000
+  const expiresAt = new Date(
+    new Date().getTime() + credentials.expires_in * 1000 - safetyMargin
+  ).getTime()
+  credentials.expires_at = expiresAt
+
+  try {
+    sessionStorage.setItem('credentials_embedded', JSON.stringify(credentials))
+    return credentials
+  } catch (error) {
+    return null
+  }
+}
+
+function App(props) {
+  console.log(props);
+  useEffect(() => {
+    getAuth(props.credentials)
+   
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,11 +44,11 @@ const renderApp = () => {
   const RUNNER_KEY = `${APP_ID}_runner`;
 
   global[RUNNER_KEY] = (targetElementId, config) => {
-    console.log("config", config);
+    // console.log("config", config);
     const root = ReactDOM.createRoot(document.getElementById(targetElementId));
     root.render(
       <React.StrictMode>
-        <App />
+        <App {...config}/>
       </React.StrictMode>
     );
   };
