e488edd2da7bdce2117c322eb3d6092f0520bee5
Filipe Raiz <filipe.raiz@unico.io>
2023-08-22 03:03:21 -0300

    sign page

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,30 @@ import logo from "./logo.svg";
 
 const HomePage = () => <h2>Home Page</h2>;
 const AboutPage = () => <h2>About Page</h2>;
-const SignPage = () => <h2>Sign Page</h2>;
+
+const SignPage = () => {
+  const handleEmbeddedApp = () => {
+    const embeddedConfig = {
+      appId: "sign_b2b_dashboard",
+      urlManifest: "http://localhost:8000",
+      renderTimeout: 600,
+    };
+
+    window.embeddedApp(embeddedConfig, {
+      route: "/template",
+      credentials: {
+        access_token: "",
+        expires_in: "7572",
+      },
+    });
+  };
+
+  React.useEffect(() => {
+    handleEmbeddedApp();
+  }, []);
+
+  return <div id="sign_b2b_dashboard"></div>;
+};
 
 function App() {
   return (
