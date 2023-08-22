769cbc5f3716ccdb3c879a314db147b7bc4c4db9
Filipe Raiz <filipe.raiz@unico.io>
2023-08-22 02:56:01 -0300

    router

--- src/index.jsx
+++ src/index.jsx
@@ -1,61 +1,47 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import "./index.css";
 
 import logo from "./logo.svg";
 
-const getAuth = async (credentials) => {
-  const safetyMargin = 5 * 60 * 1000
-  const expiresAt = new Date(
-    new Date().getTime() + credentials.expires_in * 1000 - safetyMargin
-  ).getTime()
-  credentials.expires_at = expiresAt
+const HomePage = () => <h2>Home Page</h2>;
+const AboutPage = () => <h2>About Page</h2>;
+const SignPage = () => <h2>Sign Page</h2>;
 
-  try {
-    sessionStorage.setItem('credentials_embedded', JSON.stringify(credentials))
-    return credentials
-  } catch (error) {
-    return null
-  }
-}
-
-function App(props) {
-  console.log(props);
-  useEffect(() => {
-    getAuth(props.credentials)
-   
-  }, [])
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Header App</h2>
-      </header>
-      <main>
-        <h2>Main App</h2>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="embedded-app">
+        <div className="embedded-header">
+          <img src={logo} className="embedded-logo" alt="logo" />
+
+          <nav className="embedded-menu">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/sign">Sign</Link>
+          </nav>
+        </div>
+        <div className="embedded-main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/sign" element={<SignPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
 const renderApp = () => {
-  const APP_ID = "embedded_app";
-  const RUNNER_KEY = `${APP_ID}_runner`;
+  const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  global[RUNNER_KEY] = (targetElementId, config) => {
-    // console.log("config", config);
-    const root = ReactDOM.createRoot(document.getElementById(targetElementId));
-    root.render(
-      <React.StrictMode>
-        <App {...config}/>
-      </React.StrictMode>
-    );
-  };
-
-  if (!global.embedded) {
-    window[RUNNER_KEY]("root");
-  }
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 };
 
 renderApp();
